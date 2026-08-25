711c64d1dfc767e8fea70082779fefff7c29dcdb
moraakshaya <moraakshayareddy@gmail.com>
2026-08-25 10:59:32 +0530

    make ui updates

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -6,7 +6,7 @@ const DashboardLayout = () => {
     return (
         <div className="flex h-screen overflow-hidden bg-white">
             <Sidebar />
-            
+
             {/* Main Content Area */}
             <div className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
                 <Outlet />
